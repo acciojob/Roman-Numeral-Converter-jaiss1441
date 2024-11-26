8ede9578b080d051cdde27948cd5f8ec6d76765d
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-27 00:41:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,15 +11,17 @@ function convertToRoman(num) {
 
 	  let result = "";
 
-	for (let i = 0; i < obj.length; i++) {
-    // While num is greater than or equal to the value of the current Roman numeral
-    while (num >= obj[i][1]) {
-      result += obj[i][0]; // Append the Roman numeral symbol to the result
-      num -= obj[i][1];   // Subtract the value from num
+    // Loop through the object array
+    for (let i = 0; i < obj.length; i++) {
+        // While num is greater than or equal to the value of the current Roman numeral
+        while (num >= obj[i][1]) {
+            result += obj[i][0]; // Append the Roman numeral symbol to the result
+            num -= obj[i][1];   // Subtract the value from num
+        }
     }
-  }
 
-console.log(result);
+    return result;
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
