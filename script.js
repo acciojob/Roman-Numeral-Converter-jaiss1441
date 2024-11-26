5de7b9db1e4d6860a749e1caf5dace5dba014806
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-27 00:38:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function convertToRoman(num) {
     }
   }
 
-  return result;
+console.log(result);
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
