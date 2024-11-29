d1ce20c85ed7b8a6ca9342f047faebaff9a0b6f3
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-30 02:20:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function convertToRoman(num) {
   	 const romanSymbols = [
     ['M', 1000],
-    ['CM', 900],
     ['D', 500],
-    ['CD', 400],
     ['C', 100],
-    ['XC', 90],
     ['L', 50],
-    ['XL', 40],
     ['X', 10],
     ['V', 5],
-    ['IV', 4],
     ['I', 1]
   ];
 
